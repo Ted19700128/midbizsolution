f3ae9c429f5d78a8e5498e32184cc36bcb9de428
Ted19700128 <eth015b@gmail.com>
2024-09-18 18:57:30 +0900

    update templateㄴ

--- myapp/forms.py
+++ myapp/forms.py
@@ -18,6 +18,18 @@ class EquipmentForm(forms.ModelForm):
             'specs': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
         }
 
+        labels = {
+            'equipment_number': '설비 번호',
+            'name': '설비 이름',
+            'model_name': '모델명',
+            'manufacturer': '제조사',
+            'mfg_date': '제조 일자',
+            'mfg_number': '제조 번호',
+            'equipment_type': '형식',
+            'specs': '사양',
+        }
+
+
     def as_custom(self, exclude_fields=None):
         if exclude_fields is None:
             exclude_fields = []
