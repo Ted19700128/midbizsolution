ba62ff17e0c2efc018bd9e3375dda2f1c0c88f45
Ted19700128 <eth015b@gmail.com>
2024-09-19 20:11:34 +0900

    modify pfcstandard/urls.py

--- pfcstandard/urls.py
+++ pfcstandard/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('create/', views.create_pfcs, name='create_pfcs'),
     path('searchpopup/', views.show_search_popup, name='show_search_popup'),
+    path('menu/', views.pfcs_menu, name='pfcs_menu'),  # pfcs_menu 뷰 함수와 URL 이름 설정
 ]
