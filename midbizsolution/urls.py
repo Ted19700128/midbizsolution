95cd90bc77fe856f7b94175fd7bb6208a9632712
Eum <TEUM@hanonsystems.com>
2024-09-23 16:09:55 +0900

    update urls.py

--- midbizsolution/urls.py
+++ midbizsolution/urls.py
@@ -1,11 +1,13 @@
 # D:\web\midbizsolution\midbizsolution\urls.py
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views  # views.py 파일에서 함수들을 import
 
 urlpatterns = [
     path('secure-admin/', admin.site.urls),
+    path('pemledger/', include('pemledger.urls')),
+    path('pecstandard/', include('pecstandard.urls')),
     path('', views.home, name='home'),  # 루트 URL에 'home' 뷰 함수 연결, base.html 렌더링
     path('greetings/', views.greetings, name='greetings'),  # greetings.html 렌더링
     path('checkpoint/', views.checkpoint, name='checkpoint'),  # checkpoint.html 렌더링
